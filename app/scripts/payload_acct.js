a0337f11f762016b31eb09960d063a543aaea685
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-18 14:29:18 -0400

    README set-up and Payloads created

---
+++ app/scripts/payload_acct.js
@@ -0,0 +1,36 @@
+//   https://api.github.com/users/lpatti10
+
+var ghAcct = {
+  {
+    "login": "lpatti10",
+    "id": 6626282,
+    "avatar_url": "https://avatars.githubusercontent.com/u/6626282?",
+    "gravatar_id": "ed1b2ac45047cab95773fd6101074d8c",
+    "url": "https://api.github.com/users/lpatti10",
+    "html_url": "https://github.com/lpatti10",
+    "followers_url": "https://api.github.com/users/lpatti10/followers",
+    "following_url": "https://api.github.com/users/lpatti10/following{/other_user}",
+    "gists_url": "https://api.github.com/users/lpatti10/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/lpatti10/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/lpatti10/subscriptions",
+    "organizations_url": "https://api.github.com/users/lpatti10/orgs",
+    "repos_url": "https://api.github.com/users/lpatti10/repos",
+    "events_url": "https://api.github.com/users/lpatti10/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/lpatti10/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Laura Patti",
+    "company": "",
+    "blog": "http://laurapatti.com",
+    "location": "United States",
+    "email": "lpatti10@gmail.com",
+    "hireable": false,
+    "bio": null,
+    "public_repos": 16,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 4,
+    "created_at": "2014-02-08T18:36:57Z",
+    "updated_at": "2014-06-18T02:34:15Z"
+  }
+}
